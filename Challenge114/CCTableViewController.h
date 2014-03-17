d967fb7c7d7a613e3c3805837c22e2b1b802a7a7
Piers Rudgard-Redsell <piersrr@gmail.com>
2014-03-17 14:36:24 +0000

    made new files

---
+++ Challenge114/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  Challenge114
+//
+//  Created by Piers Rudgard-Redsell on 17/03/2014.
+//  Copyright (c) 2014 Airbyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
